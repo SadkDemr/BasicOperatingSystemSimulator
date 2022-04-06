eea5b48b9cb62cf0a0cf0b13b2c3219d009d4c8f
SadkDemr <65198970+SadkDemr@users.noreply.github.com>
2022-04-06 22:14:24 +0300

    BasicOperatingSystemSimulator
    Basic Operating System Simulator

---
+++ BasicOperatingSystemSimulator/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(){
+time_t s, val = 1;
+struct tm* current_time;
+s = time(NULL);
+current_time = localtime(&s);
+
+//parametreler
+const __UINT32_TYPE__ OLAY_SAYISI= 10;
+const __UINT32_TYPE__ OLAY_TIPI_SAYISI=5;
+
+enum olayTipi{
+
+SUREC_BASLA,
+SISTEM_CAGRISI,
+SISTEM_BELIRLILIK,
+SUREC_BIT,
+};
+char *olayAdlari [OLAY_TIPI_SAYISI];
+olayAdlari [SUREC_BASLA] = "Surec Basladi";
+olayAdlari [SISTEM_CAGRISI] = "Sistem Cagrisi Geldi";
+olayAdlari [SISTEM_BELIRLILIK] = "Sistem Cagrisi Geldi";
+olayAdlari [SUREC_BIT] = "Surec Bitti";
+
+
+
+//olaylar dizisi
+enum olayTipi olaylar[OLAY_SAYISI];
+
+//rastgele olaylar üret
+srand(time(0));
+for (int i = 0; i < OLAY_SAYISI; i++)
+{
+    olaylar[i] = rand()%OLAY_TIPI_SAYISI;
+}
+
+
+for (int i = 0; i < OLAY_SAYISI; i++)
+{
+   printf("Olay: %d\n",olaylar[i]);
+   printf("Olay: %s\n\n",olayAdlari[olaylar[i]]);
+   printf("Sistem Suresi:\n");
+
+    printf("Saat:%02d:%02d:%02d\n",
+           current_time->tm_hour,
+           current_time->tm_min,
+           current_time->tm_sec);
+
+}
+//Olay belleği
+printf("Tum olaylar(Bellek):");
+return (0);
+}
+
+
